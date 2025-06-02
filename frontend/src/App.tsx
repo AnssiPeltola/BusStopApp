42f9aa1b1bff2b63adc15d31419590cefd6fc948
Anssi Peltola <anssipeltola@hotmail.com>
2025-06-02 18:10:10 +0300

    Start of the project
    Made simple backend with node.js/Express.js and React frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((response) => setMessage(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>BusStopApp</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
